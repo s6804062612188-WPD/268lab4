982c6b13a266040f33eaa9b76a60bfe4adb4d010
Wongsathon Panthudech <s6804062612188@email.kmutnb.ac.th>
2025-12-24 00:34:44 +0700

    display square of a character version 2
    time complexity is still O(n) = n

--- lab4no6.cpp
+++ lab4no6.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	char n; int r;
-	scanf("%s %d", &n,&r);
+	char n; int r, c;
+	scanf("%c %d %d", &n,&c,&r);
 	char s[r];
-	for (int i=0; i<r; i++) {
+	for (int i=0; i<c; i++) {
 		s[i] = n;
 	}
 	for (int i=0; i<r; i++) {
